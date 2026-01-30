fd08428ce9b457a50a6938a0c78ee397d440da26
coldsteeze <nikitaproduction@mail.ru>
2026-01-30 11:41:29 +0300

    style(user-profile): fix formatting in NicknameAlreadyTakenException

--- user-profile-service/src/main/java/korobkin/nikita/user_profile_service/exception/NicknameAlreadyTakenException.java
+++ user-profile-service/src/main/java/korobkin/nikita/user_profile_service/exception/NicknameAlreadyTakenException.java
@@ -1,7 +1,7 @@
 package korobkin.nikita.user_profile_service.exception;
 
 public class NicknameAlreadyTakenException extends AppException {
-  public NicknameAlreadyTakenException(ErrorCode errorCode) {
-    super(errorCode);
-  }
+    public NicknameAlreadyTakenException(ErrorCode errorCode) {
+        super(errorCode);
+    }
 }
