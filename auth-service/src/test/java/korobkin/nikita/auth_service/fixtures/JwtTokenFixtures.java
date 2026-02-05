08f7ff58f62016557c140aee5fe2182c00ce03c1
coldsteeze <nikitaproduction@mail.ru>
2026-02-05 16:00:32 +0300

    feat(auth-service): add methods to create valid and expired JWT refresh tokens

--- auth-service/src/test/java/korobkin/nikita/auth_service/fixtures/JwtTokenFixtures.java
+++ auth-service/src/test/java/korobkin/nikita/auth_service/fixtures/JwtTokenFixtures.java
@@ -1,7 +1,13 @@
 package korobkin.nikita.auth_service.fixtures;
 
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
 import korobkin.nikita.auth_service.dto.internal.JwtTokens;
+import korobkin.nikita.auth_service.security.jwt.JwtProperties;
 import lombok.experimental.UtilityClass;
+
+import java.util.Date;
+import java.util.UUID;
 
 @UtilityClass
 public class JwtTokenFixtures {
@@ -18,5 +24,25 @@ public class JwtTokenFixtures {
 
     public static JwtTokens jwtTokens() {
         return jwtTokens(DEFAULT_ACCESS_TOKEN, DEFAULT_REFRESH_TOKEN, DEFAULT_EXPIRES_IN);
+    }
+
+    public static String createValidRefreshToken(JwtProperties jwtProperties) {
+        return JWT.create()
+                .withSubject(UUID.randomUUID().toString())
+                .withClaim("type", "refresh_token")
+                .withIssuer(jwtProperties.getIssuer())
+                .withIssuedAt(new Date())
+                .withExpiresAt(new Date(System.currentTimeMillis() + 3600000))
+                .sign(Algorithm.HMAC256(jwtProperties.getSecret()));
+    }
+
+    public static String createExpiredRefreshToken(JwtProperties jwtProperties) {
+        return JWT.create()
+                .withSubject(UUID.randomUUID().toString())
+                .withClaim("type", "refresh_token")
+                .withIssuer(jwtProperties.getIssuer())
+                .withIssuedAt(new Date())
+                .withExpiresAt(new Date(System.currentTimeMillis() - 1000))
+                .sign(Algorithm.HMAC256(jwtProperties.getSecret()));
     }
 }
