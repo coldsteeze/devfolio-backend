5f982eee2c0428e30dbb07fab02f850aaef823a4
coldsteeze <nikitaproduction@mail.ru>
2026-02-17 10:33:49 +0300

    feat(jwt-starter): JwtService logic moved to starter and role extraction added

---
+++ jwt-security-starter/src/main/java/korobkin/nikita/jwtsecuritystarter/security/jwt/JwtServiceImpl.java
@@ -0,0 +1,52 @@
+package korobkin.nikita.jwtsecuritystarter.security.jwt;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.auth0.jwt.interfaces.DecodedJWT;
+import korobkin.nikita.jwtsecuritystarter.config.JwtProperties;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+public class JwtServiceImpl implements JwtService {
+
+    public static final String ACCESS_TOKEN_TYPE = "access_token";
+    private final JwtProperties jwtProperties;
+
+    public JwtServiceImpl(JwtProperties jwtProperties) {
+        this.jwtProperties = jwtProperties;
+    }
+
+    @Override
+    public DecodedJWT verifyAccessToken(String token) throws JWTVerificationException {
+        DecodedJWT jwt = JWT.require(Algorithm.HMAC256(jwtProperties.getSecret()))
+                .withIssuer(jwtProperties.getIssuer())
+                .build()
+                .verify(token);
+
+        if (!ACCESS_TOKEN_TYPE.equals(jwt.getClaim("type").asString())) {
+            throw new JWTVerificationException("Invalid token type. Expected: " + ACCESS_TOKEN_TYPE);
+        }
+
+        return jwt;
+    }
+
+    @Override
+    public UUID getUserIdFromVerifiedToken(DecodedJWT jwt) {
+        return UUID.fromString(jwt.getSubject());
+    }
+
+    @Override
+    public String getEmailFromVerifiedToken(DecodedJWT jwt) {
+        return jwt.getClaim("email").asString();
+    }
+
+    @Override
+    public List<String> getRolesFromVerifiedToken(DecodedJWT jwt) {
+        return Optional.ofNullable(jwt.getClaim("role").asList(String.class))
+                .orElse(Collections.emptyList());
+    }
+}
