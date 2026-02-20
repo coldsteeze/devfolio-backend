97301e7689b114ddf737911dd6bbc5b123137783
coldsteeze <nikitaproduction@mail.ru>
2026-02-20 23:36:42 +0300

    feat(skill): add @NotBlank validation to UpdateSkillRequest

--- skill-service/src/main/java/korobkin/nikita/skill_service/dto/request/UpdateSkillRequest.java
+++ skill-service/src/main/java/korobkin/nikita/skill_service/dto/request/UpdateSkillRequest.java
@@ -1,6 +1,7 @@
 package korobkin.nikita.skill_service.dto.request;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import korobkin.nikita.skill_service.entity.enums.SkillCategory;
 import lombok.Getter;
@@ -11,6 +12,7 @@ import lombok.Setter;
 @Schema(description = "Request dto for update skill")
 public class UpdateSkillRequest {
 
+    @NotBlank(message = "Skill name is required")
     @Size(min = 2, max = 50, message = "Skill name must be between 2 and 50 characters")
     @Schema(example = "Java", description = "Field for skill name")
     private String name;
