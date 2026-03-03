0609f3d3ed64623c3260d699faf2a0180888596d
coldsteeze <nikitaproduction@mail.ru>
2026-03-03 15:50:17 +0300

    feat(project): add ProjectController, UserProjectController

---
+++ project-service/src/main/java/korobkin/nikita/project_service/controller/UserProjectController.java
@@ -0,0 +1,30 @@
+package korobkin.nikita.project_service.controller;
+
+import korobkin.nikita.project_service.dto.response.ProjectResponse;
+import korobkin.nikita.project_service.security.user.UserPrincipal;
+import korobkin.nikita.project_service.service.ProjectService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/users")
+public class UserProjectController {
+
+    private final ProjectService projectService;
+
+    @GetMapping("/{userId}/projects")
+    public ResponseEntity<List<ProjectResponse>> getUserProjects(
+            @PathVariable("userId") UUID userId,
+            @AuthenticationPrincipal UserPrincipal currentUser) {
+        return ResponseEntity.ok(projectService.getUserProjects(userId, currentUser));
+    }
+}
