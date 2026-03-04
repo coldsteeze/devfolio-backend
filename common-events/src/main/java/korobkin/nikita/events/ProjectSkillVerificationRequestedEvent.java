0847dce1d57de9860a8e86bfb403c9800423df72
coldsteeze <nikitaproduction@mail.ru>
2026-03-04 12:44:03 +0300

    feat(project): add skillName, GithubUrl in common event ProjectSkillVerificationRequestedEvent

--- common-events/src/main/java/korobkin/nikita/events/ProjectSkillVerificationRequestedEvent.java
+++ common-events/src/main/java/korobkin/nikita/events/ProjectSkillVerificationRequestedEvent.java
@@ -2,5 +2,6 @@ package korobkin.nikita.events;
 
 import java.util.UUID;
 
-public record ProjectSkillVerificationRequestedEvent(UUID projectId, UUID skillId) {
+public record ProjectSkillVerificationRequestedEvent(
+        UUID projectId, UUID skillId, String skillName, String githubUrl) {
 }
