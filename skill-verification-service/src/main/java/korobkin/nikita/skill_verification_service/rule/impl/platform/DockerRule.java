fcc16c3d695819d99c21a2b889e9fa7b76c01adc
coldsteeze <nikitaproduction@mail.ru>
2026-04-08 12:35:00 +0300

    feat(skill-verification): change logic for parsing Docker and Kafka

--- skill-verification-service/src/main/java/korobkin/nikita/skill_verification_service/rule/impl/platform/DockerRule.java
+++ skill-verification-service/src/main/java/korobkin/nikita/skill_verification_service/rule/impl/platform/DockerRule.java
@@ -8,11 +8,23 @@ import org.springframework.stereotype.Component;
 @Component
 public class DockerRule implements SkillVerificationRule {
 
+    @Override
     public boolean supports(SkillShortInfo skill) {
         return skill.name().equalsIgnoreCase("Docker");
     }
 
+    @Override
     public boolean verify(SkillShortInfo skill, ProjectData data) {
-        return data.fileExists("Dockerfile");
+
+        if (data.fileExists("Dockerfile")
+                || data.fileExists("docker-compose.yml")) {
+            return true;
+        }
+
+        return data.getRootDirectories().stream()
+                .anyMatch(dir ->
+                        data.fileExists(dir + "/Dockerfile") ||
+                                data.fileExists(dir + "/docker-compose.yml")
+                );
     }
 }
--- skill-verification-service/src/main/java/korobkin/nikita/skill_verification_service/rule/impl/platform/KafkaRule.java
+++ skill-verification-service/src/main/java/korobkin/nikita/skill_verification_service/rule/impl/platform/KafkaRule.java
@@ -1,41 +1,18 @@
 package korobkin.nikita.skill_verification_service.rule.impl.platform;
 
-import korobkin.nikita.events.skill.SkillShortInfo;
-import korobkin.nikita.skill_verification_service.model.ProjectData;
-import korobkin.nikita.skill_verification_service.rule.SkillVerificationRule;
+import korobkin.nikita.skill_verification_service.rule.FileContentRule;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
 
 @Component
-public class KafkaRule implements SkillVerificationRule {
+public class KafkaRule extends FileContentRule {
 
-    @Override
-    public boolean supports(SkillShortInfo skill) {
-        return skill.name().equalsIgnoreCase("Kafka");
-    }
-
-    @Override
-    public boolean verify(SkillShortInfo skill, ProjectData data) {
-        List<String> services = data.listDirectories();
-
-        for (String servicePath : services) {
-            boolean javaKafka = data.getFileContent(servicePath + "/pom.xml")
-                    .map(c -> c.toLowerCase().contains("kafka"))
-                    .orElse(false);
-            if (javaKafka) return true;
-
-            boolean pythonKafka = data.getFileContent(servicePath + "/requirements.txt")
-                    .map(c -> c.toLowerCase().contains("kafka"))
-                    .orElse(false);
-            if (pythonKafka) return true;
-
-            boolean jsKafka = data.getFileContent(servicePath + "/package.json")
-                    .map(c -> c.toLowerCase().contains("kafka"))
-                    .orElse(false);
-            if (jsKafka) return true;
-        }
-
-        return false;
+    public KafkaRule() {
+        super(
+                "Kafka",
+                List.of("pom.xml", "build.gradle", "requirements.txt", "package.json"),
+                List.of("kafka", "spring-kafka", "kafkajs", "confluent-kafka")
+        );
     }
 }
