8ac0123000ff017bce837ab5d492be208edf2c28
coldsteeze <nikitaproduction@mail.ru>
2026-04-13 18:11:50 +0300

    feat(portfolio): add Kafka consumer for project-skill-removed topic

---
+++ portfolio-service/src/main/java/korobkin/nikita/portfolio_service/kafka/consumer/skill/ProjectSkillRemovedListener.java
@@ -0,0 +1,25 @@
+package korobkin.nikita.portfolio_service.kafka.consumer.skill;
+
+import korobkin.nikita.events.ProjectSkillRemovedEvent;
+import korobkin.nikita.portfolio_service.service.PortfolioService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class ProjectSkillRemovedListener {
+
+    private final PortfolioService portfolioService;
+
+    @KafkaListener(
+            topics = "#{@kafkaTopicProperties.projectSkillRemoved}",
+            containerFactory = "kafkaListenerContainerFactory"
+    )
+    public void handleProjectSkillRemoved(ProjectSkillRemovedEvent event) {
+        log.info("received ProjectSkillRemovedEvent: {}", event);
+        portfolioService.deletePortfolioProjectSkill(event);
+    }
+}
