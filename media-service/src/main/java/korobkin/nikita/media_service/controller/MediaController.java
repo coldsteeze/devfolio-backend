96f198cc1b272a58a01ca67eb828c05c78493a89
coldsteeze <nikitaproduction@mail.ru>
2026-04-26 22:47:58 +0300

    feat(media): add MediaController for upload, delete photos

---
+++ media-service/src/main/java/korobkin/nikita/media_service/controller/MediaController.java
@@ -0,0 +1,34 @@
+package korobkin.nikita.media_service.controller;
+
+import korobkin.nikita.media_service.dto.DeleteMediaRequest;
+import korobkin.nikita.media_service.dto.MediaResponse;
+import korobkin.nikita.media_service.service.MediaService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+@RestController
+@RequestMapping("/api/media")
+@RequiredArgsConstructor
+public class MediaController {
+
+    private final MediaService mediaService;
+
+    @PostMapping(value = "/upload", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<MediaResponse> upload(
+            @RequestPart("file") MultipartFile file,
+            @RequestParam("folder") String folder
+    ) {
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(mediaService.upload(file, folder));
+    }
+
+    @DeleteMapping
+    public ResponseEntity<Void> delete(@RequestBody DeleteMediaRequest request) {
+        mediaService.delete(request.url());
+        return ResponseEntity.noContent().build();
+    }
+}
