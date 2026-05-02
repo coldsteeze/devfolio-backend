fdadbcb1961e9848cb2e466161b4eda5e7cbce9f
coldsteeze <nikitaproduction@mail.ru>
2026-05-02 12:20:02 +0300

    feat(project): add upload preview photo endpoint in ProjectController

--- project-service/src/main/java/korobkin/nikita/project_service/controller/ProjectController.java
+++ project-service/src/main/java/korobkin/nikita/project_service/controller/ProjectController.java
@@ -5,15 +5,18 @@ import korobkin.nikita.project_service.docs.ProjectControllerDocs;
 import korobkin.nikita.project_service.dto.request.CreateProjectRequest;
 import korobkin.nikita.project_service.dto.request.UpdateProjectRequest;
 import korobkin.nikita.project_service.dto.response.*;
+import korobkin.nikita.project_service.dto.response.media.MediaResponse;
 import korobkin.nikita.project_service.security.user.UserPrincipal;
 import korobkin.nikita.project_service.service.ProjectService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 import java.util.UUID;
@@ -84,8 +87,8 @@ public class ProjectController implements ProjectControllerDocs {
 
     @GetMapping("/{projectId}/skills")
     public ResponseEntity<List<ProjectSkillResponse>> getProjectSkills(
-        @PathVariable("projectId") UUID projectId,
-        @AuthenticationPrincipal UserPrincipal currentUser) {
+            @PathVariable("projectId") UUID projectId,
+            @AuthenticationPrincipal UserPrincipal currentUser) {
         return ResponseEntity.ok(projectService.getProjectSkills(projectId, currentUser));
     }
 
@@ -93,5 +96,13 @@ public class ProjectController implements ProjectControllerDocs {
     public ResponseEntity<PagedResponse<ProjectFeedResponse>> getProjectsFeed(
             @PageableDefault(size = 10) Pageable pageable) {
         return ResponseEntity.ok(projectService.getProjectsFeed(pageable));
+    }
+
+    @PostMapping(value = "/{projectId}/preview", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<MediaResponse> uploadPreviewPhoto(
+            @PathVariable("projectId") UUID projectId,
+            @AuthenticationPrincipal UserPrincipal currentUser,
+            @RequestPart("file") MultipartFile file) {
+        return ResponseEntity.ok(projectService.uploadPreviewPhoto(projectId, currentUser, file));
     }
 }
