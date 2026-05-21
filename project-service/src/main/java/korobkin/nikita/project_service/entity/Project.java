65e6fd1c7911d35231842e68739f8f700a81a788
coldsteeze <nikitaproduction@mail.ru>
2026-05-21 13:35:32 +0300

    feat(project): initialize count likes, views in Project entity, update ProjectResponse: add countLikes, countViews

--- project-service/src/main/java/korobkin/nikita/project_service/entity/Project.java
+++ project-service/src/main/java/korobkin/nikita/project_service/entity/Project.java
@@ -55,10 +55,10 @@ public class Project {
     private List<ProjectImage> images = new ArrayList<>();
 
     @Column(name = "views_count", nullable = false)
-    private Long viewsCount;
+    private Long viewsCount = 0L;
 
     @Column(name = "likes_count", nullable = false)
-    private Long likesCount;
+    private Long likesCount = 0L;
 
     @Column(name = "created_at", nullable = false, updatable = false)
     private LocalDateTime createdAt;
