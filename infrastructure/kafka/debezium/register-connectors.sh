da5dbd6b5b3bd22a36f469366cb08f8ccbe06e33
coldsteeze <nikitaproduction@mail.ru>
2026-05-28 15:22:38 +0300

    chore: project-outbox-connector.json in register-connectors.sh

--- infrastructure/kafka/debezium/register-connectors.sh
+++ infrastructure/kafka/debezium/register-connectors.sh
@@ -11,3 +11,9 @@ echo "Registering profile connector..."
 curl -X POST http://localhost:8087/connectors \
   -H "Content-Type: application/json" \
   -d @./connectors/user-profile-outbox-connector.json
+
+echo "Registering project connector..."
+
+curl -X POST http://localhost:8087/connectors \
+  -H "Content-Type: application/json" \
+  -d @./connectors/project-outbox-connector.json
